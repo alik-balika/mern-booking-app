dc93860967b22f45a64ee7600ba9da9fa7ab1ff8
alik-balika <balikaalik@gmail.com>
2024-04-13 23:44:39 -0700

    add port and cloudinary

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(7000, () => {
-  console.log("server running on localhost:7000");
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
