6b2e8f8a9e0e42ce7ae3977251889a1c78be08aa
alik-balika <balikaalik@gmail.com>
2024-04-17 15:01:59 -0700

    add cloudinary integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,16 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
