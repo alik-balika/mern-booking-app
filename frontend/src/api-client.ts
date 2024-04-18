17d718884fc7c5a44c7511500c73148c8c347f32
alik-balika <balikaalik@gmail.com>
2024-04-17 22:41:24 -0700

    create endpoint to fetch users hotels. As well as api client to call it.

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,6 @@
 import { RegisterFormData } from "./pages/RegisterPage";
 import { SignInFormData } from "./pages/SignInPage";
+import { HotelType } from "../../backend/src/models/hotel";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -70,6 +71,18 @@ export const addMyHotel = async (hotelFormData: FormData) => {
 
   if (!response.ok) {
     throw new Error("Failed to add hotel");
+  }
+
+  return response.json();
+};
+
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
   }
 
   return response.json();
